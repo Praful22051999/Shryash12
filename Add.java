18fa79e9e81155dab897b014a6a5892e961bc039
Praful22051999 <136887238+Praful22051999@users.noreply.github.com>
2023-06-18 15:01:41 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+package org.techhub;
+
+class A{
+	static int m=100;
+	void show() {
+		System.out.println("M is  "+m);
+	}
+}
+public class StatVarApp {
+  public static void main(String[] args) {
+      new A().show();
+	}
+}
+
